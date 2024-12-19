f791a98376aa1d00f43d8fe5ec7dd152310aa057
Flavjuni98 <flavjuni@outlook.com>
2024-12-19 10:29:42 -0300

    Desafio de Projeto

---
+++ Desafio_dio.js
@@ -0,0 +1,14 @@
+let nomeHeroi = "Kratos"
+
+let experienciaXP = 10001
+
+if (experienciaXP < 1000) console.log(nomeHeroi + " é nivel Ferro");
+    
+
+if (experienciaXP >=1000 && experienciaXP < 2000) console.log(nomeHeroi + " é nivel Bronze")
+if (experienciaXP >=2001 && experienciaXP < 5000) console.log(nomeHeroi + " é nivel Prata")
+if (experienciaXP >=5001 && experienciaXP < 7000) console.log(nomeHeroi + " é nivel Ouro")
+if (experienciaXP >=7001 && experienciaXP < 8000) console.log(nomeHeroi + " é nivel Platina")
+if (experienciaXP >=8001 && experienciaXP < 9000) console.log(nomeHeroi + " é nivel Ascendente")
+if (experienciaXP >=9001 && experienciaXP < 10000) console.log(nomeHeroi + " é nivel Imortal")
+if (experienciaXP >=10001) console.log(nomeHeroi + " é nivel Radiante")
